d1a148f64c4cca47edac06ab1dc0e28a9859ff68
PrimalCat-Real <romanenko.denis2003@gmail.com>
2026-02-16 21:49:01 +0100

    add mask, change gradient

--- src/modules/primalui/components/TestButton.tsx
+++ src/modules/primalui/components/TestButton.tsx
@@ -5,8 +5,9 @@ const TestButton = () => {
     const buttonName = "TestButton"
     return (
         <button aria-label={buttonName} className={cn('px-4 py-2 border-blue-400/40 border rounded-md cursor-pointer relative overflow-hidden',
-            'after:bg-conic-30 after:from-transparent after:via-blue-400 after:to-transparent after:w-[150%] after:block after:aspect-square after:content-[""] after:absolute after:-translate-x-1/2 after:left-1/2 after:top-1/2 after:-translate-y-1/2 after:opacity-50  after:animate-[spin_3s_linear_infinite]'
-        )}>{buttonName}</button>
+            'after:mask-[radial-gradient(circle_at_bottom,transparent_40%,black)] after:bg-linear-300 after:from-transparent after:via-blue-400 after:to-transparent after:w-[150%] after:block after:aspect-square after:content-[""] after:absolute after:-translate-x-1/2 after:left-1/2 after:top-1/2 after:-translate-y-1/2 after:opacity-60  after:animate-[spin_3s_linear_infinite]'
+        )}>{buttonName}
+        </button>
     )
 }
 
