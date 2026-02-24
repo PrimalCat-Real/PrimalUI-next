3bcf7ea62d03fb5c98a09ba7008f2e879ff5b1e2
PrimalCat-Real <romanenko.denis2003@gmail.com>
2026-02-24 17:38:00 +0100

    fix border to 1 px

--- src/modules/primalui/components/aurora/GlassGradinetBars.tsx
+++ src/modules/primalui/components/aurora/GlassGradinetBars.tsx
@@ -14,7 +14,7 @@ const GlassGradientBars = ({ bars = 13, children }: GlassGradientBarsProps) => {
             <div className="flex h-full w-full absolute inset-0">
                 {Array.from({ length: bars }).map((_, index) => (
                     <div className='relative flex flex-1'>
-                        <div className="absolute inset-0 border-x border-white/10 backdrop-blur-[140px] bg-linear-to-r from-white/25 via-28% via-black/20 to-white/25 bg-blend-overlay" />
+                        <div className="absolute inset-0 first:border-x border-r border-white/10 backdrop-blur-[140px] bg-linear-to-r from-white/25 via-28% via-black/20 to-white/25 bg-blend-overlay" />
                     </div>
 
                 ))}
